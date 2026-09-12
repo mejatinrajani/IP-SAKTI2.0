d1fc79039faa87478c3b3974efecf84ef66b5aaf
Jatin Rajani <mejatinrajani@gmail.com>
2026-09-13 02:54:10 +0530

    a

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { AuthProvider, useAuth } from './context/AuthContext';
+import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import EvaluatorView from './components/EvaluatorView';
 import ABSCalculator from './components/ABSCalculator';
@@ -75,11 +76,17 @@ function MainLayout() {
         onUpdateTitle={handleUpdateTitle}
         onTogglePin={handleTogglePin}
         onDeleteChat={handleDeleteChat}
-        mainView={mainView} 
-        setMainView={setMainView}
       />
 
       <div className="flex-1 flex flex-col h-full overflow-hidden bg-white">
+        <Navbar
+          mainView={mainView}
+          setMainView={setMainView}
+          language={language}
+          setLanguage={setLanguage}
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
 
         <main className="flex-1 p-4 md:p-6 overflow-y-auto flex flex-col items-center justify-center">
           {mainView === 'evaluator' ? (
